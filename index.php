9eaabf1094e4b5191e9d0bb578120da53155159f
tealLeaf <bequynh09@gmail.com>
2018-05-12 15:02:21 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<!-- CSS  -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css"> <!--Import materialize.css-->
+	<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> <!-- Our own styling -->
+	<!-- <link rel="shortcut icon" type="image/png" href="img/favicon.png"/> --> <!-- FAVICON FOR TAB -->
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<title>Coding Log</title>
+</head>
+
+<body>
+	
+
+	<!-- SCRIPTS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
